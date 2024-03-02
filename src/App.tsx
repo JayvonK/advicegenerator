2b7e712887f420da14b42c31ca43ef83ca873f71
JayvonK <jaykhuth@gmail.com>
2024-03-01 21:29:56 -0800

    adjust

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import AdviceComponent from './Components/AdviceComponent';
-import getData from './DataServices/DataServices';
-import Advice from './Interfaces/Interface';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import AdviceComponent from "./Components/AdviceComponent";
+import getData from "./DataServices/DataServices";
+import Advice from "./Interfaces/Interface";
 
 function App() {
   const [theAdvice, setTheAdvice] = useState<Advice>();
   const [count, setCount] = useState(0);
+  const [oldAdvice, setOldAdvice] = useState<Advice>();
 
-  const changeData:() => void = () => {
-      setCount(count + 1);
-    }
+  const changeData: () => void = () => {
+    setCount(count + 1);
+  };
 
   useEffect(() => {
+    setOldAdvice(theAdvice);
     const fetchData = async () => {
       const data = await getData();
       setTheAdvice(data);
-    }
+    };
     fetchData();
-  }, [count])
+    if (oldAdvice?.slip.advice === theAdvice?.slip.advice) {
+      fetchData();
+      console.log("I ran");
+    }
+    setOldAdvice(theAdvice);
+  }, [count]);
 
   return (
     <>
-      <AdviceComponent advice={theAdvice && theAdvice.slip.advice || "empty"} id={theAdvice && theAdvice.slip.id || 0} click={changeData}/>
+      <AdviceComponent
+        advice={(theAdvice && theAdvice.slip.advice) || "empty"}
+        id={(theAdvice && theAdvice.slip.id) || 0}
+        click={changeData}
+      />
     </>
   );
 }
