43d607448cd0d20bd9f14bc1a70a4fdc2c5b2286
JayvonK <jaykhuth@gmail.com>
2024-03-01 18:42:08 -0800

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import AdviceComponent from './Components/AdviceComponent';
+import getData from './DataServices/DataServices';
+import Advice from './Interfaces/Interface';
 
 function App() {
+  const [theAdvice, setTheAdvice] = useState<Advice>()
+
+  const fetchData = async () => {
+      const data = await getData();
+      setTheAdvice(data);
+    }
+
+  useEffect(() => {
+    // fetchData();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>{theAdvice && theAdvice.slip.advice}</h1>
+    <button onClick={() => fetchData()}>Rand</button>
+      <AdviceComponent />
+    </>
   );
 }
 
---
+++ src/Components/AdviceComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdviceComponent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AdviceComponent
